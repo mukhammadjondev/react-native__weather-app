fb67850bc6f725dc6b01786a83aab2d34792dd91
mukhammadjondev <muhsdev@gmail.com>
2023-08-29 20:21:28 +0500

    weather app

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert } from 'react-native'
+import { useEffect, useState } from 'react'
+import Loader from './components/loader'
+import Weather from './components/weather'
+import * as Location from 'expo-location'
+import axios from 'axios'
+
+const API_KEY = '79c55b2c4b2772606ccd1d50eed8d89a'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [isLoading, setIsLoading] = useState(true)
+  const [location, setLocation] = useState(null)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const getWeather = async(latitude, longitude) => {
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)
+    setLocation(data)
+    setIsLoading(false)
+  }
+
+  const setWeather = async query => {
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}&units=metric`)
+    setLocation(data)
+    setIsLoading(false)
+  }
+
+  const getLocation = async() => {
+    try {
+      const {status} = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        Alert.alert('Permission to access location was denied')
+        return
+      }
+
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync({})
+      getWeather(latitude, longitude)
+    } catch (error) {
+      Alert.alert("I can't find your current location, so bad ):")
+    }
+  }
+
+  useEffect(() => {
+    getLocation()
+  }, [])
+
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <Weather temp={location.main.temp} name={location.name} condition={location.weather[0].main} setWeather={setWeather}/>
+  )
+}
---
+++ components/loader.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native'
+import AnimatedLoader from 'react-native-animated-loader'
+
+export default function Loader() {
+	return (
+		<AnimatedLoader
+			visible={true}
+			overlayColor='#FDF6AA'
+			source={require('../assets/loader.json')}
+			animationStyle={styles.lottie}
+			speed={1}
+		></AnimatedLoader>
+	)
+}
+
+const styles = StyleSheet.create({
+	lottie: {
+		width: 400,
+		height: 400,
+	},
+})
